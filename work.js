3c5758a37bad0b9b538bffa2a3866cfb46b3b04c
dmde816 <dmd816@naver.com>
2023-03-10 17:10:03 +0900

    first javascript

---
+++ work.js
@@ -0,0 +1,24 @@
+let student = ["가가", "나나", "다다"];
+let award = ["가가"];
+
+console.log(award)
+
+for(let a = 0; a < 3; a++){
+    if(student[a] == award[0]){
+        console.log(student[a])
+    }
+}
+
+
+
+for(let a = 1; a < 61; a++){
+    
+    if(a%3 > 0){
+        console.log(a)
+    }else{
+        console.log("짝")
+    }
+}
+
+
+
